35754c4f03b3a3a808c4fa93f5a050420340ca92
orange <da.naranjo10@uniandes.edu.co>
2026-03-11 20:23:03 -0500

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -25,17 +25,17 @@ output "storage_account_primary_connection_string" {
 
 output "possible_outbound_ip_addresses" {
   description = "Possible given ip address for the Azure Function Application"
-  value       = azurerm_function_app.azure_function.possible_outbound_ip_addresses
+  value       = azurerm_linux_function_app.azure_function.possible_outbound_ip_addresses
 }
 
 output "default_hostname" {
   description = "Expected URL for the Azure Function"
-  value       = azurerm_function_app.azure_function.default_hostname
+  value       = azurerm_linux_function_app.azure_function.default_hostname
 }
 
 output "id" {
   description = "ID of the Azure Function Application"
-  value       = azurerm_function_app.azure_function.id
+  value       = azurerm_linux_function_app.azure_function.id
 }
 
 output "app_service_plan_id" {
